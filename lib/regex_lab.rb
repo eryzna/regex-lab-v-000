876fde367b3e11f530fcae4cdf4f94a287f04da4
Elana Ryznar <eryzna@gmail.com>
2018-04-20 21:23:17 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,5 @@
 def starts_with_a_vowel?(word)
-  if word= /^ w\[aeiou]/
-    true
-  else word != /^ w\[aeiou]/
-    false
-  end
+  word.match(/^\w[aeiou])
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
